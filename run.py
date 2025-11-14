4c118cd5169d39564cf129529ef6f6adc8e54de9
KleoBerg <kleo.berge@gmail.com>
2025-11-14 09:44:23 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,17 @@
+from meteor import create_app
+import argparse
+parser = argparse.ArgumentParser(description='Specify config file (json)')
+parser.add_argument('--config', type=str)
+args = parser.parse_args()
+
+if args.config:
+    app = create_app(config_json=args.config)
+else:
+    app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
